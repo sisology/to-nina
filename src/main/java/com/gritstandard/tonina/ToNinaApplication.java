307cd5ddadc2bf8d4fd18264cfb3a9d6b4de8f2b
sisology <gtldhs120@gmail.com>
2025-02-14 13:05:25 +0900

    first commit

---
+++ src/main/java/com/gritstandard/tonina/ToNinaApplication.java
@@ -0,0 +1,13 @@
+package com.gritstandard.tonina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToNinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToNinaApplication.class, args);
+	}
+
+}
